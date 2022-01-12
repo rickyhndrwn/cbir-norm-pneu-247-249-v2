8d9a04f90757f8edefe375ab8321b3fdc712b11d
Ricky Hendrawan <ricky.hndrwn98@gmail.com>
2022-01-12 10:02:35 +0700

    before first request code

--- app.py
+++ app.py
@@ -1,32 +1,55 @@
+import os
+import numpy as np
 from PIL import Image
-from datetime import datetime
-from flask import Flask, request, render_template, redirect, url_for
 from pathlib import Path
 from random import shuffle
 from tensorflow.keras.models import load_model
-import numpy as np
-import os
+from flask import Flask, request, render_template
 
 from feature_extractor import FeatureExtractor
 
 app = Flask(__name__)
 
-db_model_dir = './static/db_model'
 
-# model_dn121 = load_model(db_model_dir + '/cxr_dn121.h5', compile=False)
-model_dn169 = load_model(db_model_dir + '/cxr_dn169.h5', compile=False)
-model_dn201 = load_model(db_model_dir + '/cxr_dn201.h5', compile=False)
+@app.before_first_request
+def init_db_values_func():
+    db_model_dir = './static/db_model'
+    db_feature_dir = './static/db_feature'
+    db_image_dir = './static/db_image'
+    db_test_image_dir = './static/db_test_image'
 
-db_model_dict = {
-    # 'cxr_dn121': model_dn121,
-    'cxr_dn169': model_dn169,
-    'cxr_dn201': model_dn201,
-}
+    global db_model_dict, db_feature_dict, db_img_paths, db_test_image_paths
+
+    # model_dn121 = load_model(db_model_dir + '/cxr_dn121.h5', compile=False)
+    model_dn169 = load_model(db_model_dir + '/cxr_dn169.h5', compile=False)
+    model_dn201 = load_model(db_model_dir + '/cxr_dn201.h5', compile=False)
+
+    db_model_dict = {
+        # 'cxr_dn121': model_dn121,
+        'cxr_dn169': model_dn169,
+        'cxr_dn201': model_dn201,
+    }
+
+    db_feature_dict = {
+        # 'features_dn121': db_feature_dir + '/features_dn121.npy',
+        'features_dn169': db_feature_dir + '/features_dn169.npy',
+        'features_dn201': db_feature_dir + '/features_dn201.npy',
+    }
+
+    db_img_paths = list()
+    for img_path in sorted(Path(db_image_dir).glob("*.jpeg")):
+        db_img_paths.append(img_path)
+
+    db_test_image_paths = list()
+    for img_path in sorted(Path(db_test_image_dir).glob("*.jpeg")):
+        db_test_image_paths.append(img_path)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    db_feature_dict, db_img_paths, random_test_image_dir, random_test_image_name = init_db_values_func()
+    shuffle(db_test_image_paths)
+    random_test_image_dir = db_test_image_paths[:9]
+    random_test_image_name = [img_dir.name for img_dir in random_test_image_dir]
     
     if request.method == 'POST':
         try:
@@ -46,7 +69,7 @@ def index():
         query_img.save(uploaded_img_path)
         uploaded_img_path = '../' + uploaded_img_path
 
-        # Run search
+        # Run search and gather result
         n_img = 25
         y_true = np.array([query_img_class for _ in range(n_img)], dtype=int)
 
@@ -61,8 +84,9 @@ def index():
             score_dict[db_model_dict_key] = scores
             relevant_img_count_dict[db_model_dict_key] = relevant_img_count
         
-        # Get key with best result
+        # Get key with best result and other result
         best_result_key = max(relevant_img_count_dict, key=relevant_img_count_dict.get)
+        other_result_dict = {key: relevant_img_count_dict[key] for key in relevant_img_count_dict.keys() if key != best_result_key}
         result_eval = f"{relevant_img_count_dict[best_result_key]}/{n_img}"
 
         return render_template('index.html',
@@ -72,39 +96,14 @@ def index():
                                query_image_name=query_img_name,
                                best_model=best_result_key,
                                scores=score_dict[best_result_key],
-                               eval_score=result_eval)
+                               eval_score=result_eval,
+                               other_result_dict=other_result_dict)
     
     else:
         return render_template('index.html',
                                test_images=random_test_image_dir,
                                test_images_names=random_test_image_name,
                                )
-
-
-def init_db_values_func():
-    db_image_dir = './static/db_image'
-    db_feature_dir = './static/db_feature'
-    db_test_image_dir = './static/db_test_image'
-
-    db_feature_dict = {
-        # 'features_dn121': db_feature_dir + '/features_dn121.npy',
-        'features_dn169': db_feature_dir + '/features_dn169.npy',
-        'features_dn201': db_feature_dir + '/features_dn201.npy',
-    }
-
-    db_img_paths = list()
-    for img_path in sorted(Path(db_image_dir).glob("*.jpeg")):
-        db_img_paths.append(img_path)
-
-    db_test_image_paths = list()
-    for img_path in sorted(Path(db_test_image_dir).glob("*.jpeg")):
-        db_test_image_paths.append(img_path)
-    
-    shuffle(db_test_image_paths)
-    random_test_image_dir = db_test_image_paths[:9]
-    random_test_image_name = [img_dir.name for img_dir in random_test_image_dir]
-    
-    return db_feature_dict,db_img_paths,random_test_image_dir,random_test_image_name
 
 
 def find_y_pred_func(scores):
